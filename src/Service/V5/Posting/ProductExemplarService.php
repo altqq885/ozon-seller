ed67aac224d1e911da9fa1e6b1278cf122dd465b
user <sochnev@intaro.ru>
2023-12-21 18:17:37 +0300

    /v5/fbs/posting/product/exemplar/set method

---
+++ src/Service/V5/Posting/ProductExemplarService.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gam6itko\OzonSeller\Service\V5\Posting;
+
+use Gam6itko\OzonSeller\Service\AbstractService;
+
+class ProductExemplarService extends AbstractService
+{
+    private $path = '/v5/fbs/posting/product/exemplar/';
+
+    /**
+     * @see https://docs.ozon.ru/api/seller/#operation/PostingAPI_FbsPostingProductExemplarSet
+     */
+    public function set(string $postingNumber, array $products)
+    {
+        assert(!empty($products));
+
+        $body = [
+            'posting_number' => $postingNumber,
+            'products'       => $products,
+        ];
+
+        return $this->request('POST', "{$this->path}/set", $body);
+    }
+}
